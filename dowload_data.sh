0e3998d7912f145443b44e36eb536ab8f2a756fe
Arthur <hduser@ip-172-31-45-70.eu-west-1.compute.internal>
2023-10-05 16:45:50 +0000

    :construction: WIP sample mnist autoencoder with data augmentation code
    Signed-off-by: Arthur <hduser@ip-172-31-45-70.eu-west-1.compute.internal>

--- dowload_data.sh
+++ dowload_data.sh
@@ -4,4 +4,4 @@ else
     curl -o "lfw-funneled.tgz" "http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz"
 fi
 tar zxf lfw-funneled.tgz
-hadoop fs -put lfw_funneled lfw_funneled
+hadoop fs -put lfw_funneled /lfw_funneled
